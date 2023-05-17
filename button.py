4a6c9b5dd34afb395c0c6a5a9bda4a8d80cf7511
randy huynh <huynhrand@gmail.com>
2023-05-17 09:23:43 -0700

    stuff

---
+++ button.py
@@ -0,0 +1,9 @@
+# Importing tkinter class library and renaming it as tk for ease of access 
+import tkinter as tk
+
+# Defining a function that prints "Button Clicked" when triggered
+def button_click():
+    print("Button Clicked!")
+    
+root = tk.Tk()
+root.title("Button Example")
